e240c4e86dd645c5148dd252e77f5b69242c4373
Ashes023! <ashaharyunuskhan@gmail.com>
2024-04-07 02:01:11 +0530

    Navigation

---
+++ VelocityMart/navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const navigation=() =>{
+  return (
+   <View>
+    <Text>Texttttt</Text>
+   </View>
+  )
+}
+
+export default navigation
